4f3f461e9c45015d24002f49760bf71cb7009e86
Angira Kekteeva <kira@nspcc.ru>
2022-04-27 03:03:11 +0400

    [#6] Update filling of credentials in formConf.sh
    Signed-off-by: Angira Kekteeva <kira@nspcc.ru>

--- services/s3-gw/formConf.sh
+++ services/s3-gw/formConf.sh
@@ -2,41 +2,71 @@
 
 . .env
 
-CONF_FILE="services/s3-gw/s3tests.conf"
+readonly CONF_FILE="services/s3-gw/s3tests.conf"
+readonly GATE_PUBLIC_KEY="0313b1ac3a8076e155a7e797b24f0b650cccad5941ea59d7cfd51a024a8b2a06bf"
 
 if [ -f "$CONF_FILE" ]; then
   echo "$CONF_FILE already exists"
   exit 0
 fi
 
+
 if [ ! -f "./bin/authmate" ]; then
+  echo "Downloading authmate"
   curl -L https://github.com/nspcc-dev/neofs-s3-gw/releases/download/"${S3_GW_VERSION}"/neofs-authmate-linux-amd64 --output ./bin/authmate
   if [ $? -eq 0 ]; then
     chmod +x ./bin/authmate
   else
-    echo "couldn't download authmate"
+    echo "Couldn't download authmate"
     exit 1
   fi
 fi
 
-echo "Authmate issue-secret"
-RESULT=$(AUTHMATE_WALLET_PASSPHRASE="" ./bin/authmate issue-secret --wallet ./services/s3-gw/wallet.json \
-  --peer s01.neofs.devenv:8080 --gate-public-key 0313b1ac3a8076e155a7e797b24f0b650cccad5941ea59d7cfd51a024a8b2a06bf \
-  --create-session-token --bearer-rules ./services/s3-gw/rules.json)
+issue_secret () {
+  local -n wallet=$1
+  local -n err
+  result=$(AUTHMATE_WALLET_PASSPHRASE=${wallet[password]} ./bin/authmate issue-secret --wallet ${wallet[path]} \
+  --peer s01.neofs.devenv:8080 --gate-public-key ${GATE_PUBLIC_KEY} \
+  --bearer-rules ./services/s3-gw/bearer_rules.json) 
+  echo ${result}
+}
 
-ACCESS_KEY=$(echo $RESULT | jq -r '.access_key_id')
-SECRET_KEY=$(echo $RESULT | jq -r '.secret_access_key')
+declare -A wallet_main
+wallet_main[path]="./services/s3-gw/wallets/wallet_main.json"
+wallet_main[password]=""
 
-cp services/s3tests.conf.template ${CONF_FILE}
+declare -A wallet_alt
+wallet_alt[path]="./services/s3-gw/wallets/wallet_alt.json"
+wallet_alt=""
+
+echo "Issuing a secret for main wallet"
+result_main=$(issue_secret wallet_main)
+if [ -z "$result_main" ]; then
+    exit 1
+fi
+access_key_main=$(echo ${result_main} | jq -r '.access_key_id')
+secret_key_main=$(echo ${result_main} | jq -r '.secret_access_key')
+echo "access_key=${access_key_main}, secret_key=${secret_key_main}"
+
+echo "Issuing a secret for alt wallet"
+result_alt=$(issue_secret wallet_alt)
+if [ -z "$result_alt" ]; then
+    exit 1
+fi
+access_key_alt=$(echo ${result_alt} | jq -r '.access_key_id')
+secret_key_alt=$(echo ${result_alt} | jq -r '.secret_access_key')
+echo "access_key=${access_key_alt}, secret_key=${secret_key_alt}"
+
+cp -f services/s3tests.conf.template ${CONF_FILE}
 
 sed -i "s/S3_HOST/s3.neofs.devenv/g" ${CONF_FILE}
 sed -i "s/S3_TLS/True/g" ${CONF_FILE}
 sed -i "s/S3_PORT/8080/g" ${CONF_FILE}
-sed -i "s/S3_MAIN_ACCESS_KEY/${ACCESS_KEY}/g" ${CONF_FILE}
-sed -i "s/S3_MAIN_SECRET_KEY/${SECRET_KEY}/g" ${CONF_FILE}
-sed -i "s/S3_ALT_ACCESS_KEY/${ACCESS_KEY}/g" ${CONF_FILE}
-sed -i "s/S3_ALT_SECRET_KEY/${SECRET_KEY}/g" ${CONF_FILE}
-sed -i "s/S3_TENANT_ACCESS_KEY/${ACCESS_KEY}/g" ${CONF_FILE}
-sed -i "s/S3_TENANT_SECRET_KEY/${SECRET_KEY}/g" ${CONF_FILE}
-sed -i "s/S3_IAM_ACCESS_KEY/${ACCESS_KEY}/g" ${CONF_FILE}
-sed -i "s/S3_IAM_SECRET_KEY/${SECRET_KEY}/g" ${CONF_FILE}
+sed -i "s/S3_MAIN_ACCESS_KEY/${access_key_main}/g" ${CONF_FILE}
+sed -i "s/S3_MAIN_SECRET_KEY/${secret_key_main}/g" ${CONF_FILE}
+sed -i "s/S3_ALT_ACCESS_KEY/${access_key_alt}/g" ${CONF_FILE}
+sed -i "s/S3_ALT_SECRET_KEY/${secret_key_alt}/g" ${CONF_FILE}
+sed -i "s/S3_TENANT_ACCESS_KEY/${access_key_alt}/g" ${CONF_FILE}
+sed -i "s/S3_TENANT_SECRET_KEY/${secret_key_alt}/g" ${CONF_FILE}
+sed -i "s/S3_IAM_ACCESS_KEY/${access_key_alt}/g" ${CONF_FILE}
+sed -i "s/S3_IAM_SECRET_KEY/${secret_key_alt}/g" ${CONF_FILE}
